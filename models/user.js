ff084101bc5d1633a1853c2e95d6428ef48ac5f0
Derrick Beining <derrick.beining@gmail.com>
2017-08-13 21:01:50 -0400

    all specs passed

--- models/user.js
+++ models/user.js
@@ -1,10 +1,51 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
+const Sequelize = require( 'sequelize' );
+const db = new Sequelize( 'postgres://localhost:5432/sequelize_practice', {
+  logging: false
+} );
 
-const User = db.define('user', {
-// YOUR CODE HERE...
+// MODELS ///////////////////////////////////
+const User = db.define( 'user', {
+  first: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  last: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 18
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  bio: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
 }, {
-// ...AND HERE
-});
+    // OPTIONS //////////////////////////
+    getterMethods: {
+      fullName () { return this.first + ' ' + this.last },
+    },
+    instanceMethods: {
+
+    },
+    classMethods: {
+
+    }
+  } );
+
+User.prototype.haveBirthday = function () {
+  return this.increment({ age: 1 })
+}
 
 module.exports = User;
